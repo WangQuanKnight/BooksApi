8a42b005682c4894dd705e8e2b8d14ff2b8b984b
knight <w1520138768@outlook.com>
2018-12-17 21:56:45 +0800

    the web api

---
+++ BooksApi/Services/BookService.cs
@@ -0,0 +1,56 @@
+﻿using BooksApi.Models;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BooksApi.Services
+{
+    public class BookService
+    {
+        private readonly IMongoCollection<Book> _books;
+        
+        public BookService(IConfiguration config)
+        {
+            var client = new MongoClient(config.GetConnectionString("BookstoreDb"));
+            var database = client.GetDatabase("BookstoreDb");
+            _books = database.GetCollection<Book>("Books");
+        }
+
+        public List<Book> Get()
+        {
+            return _books.Find(book => true).ToList();
+        }
+
+        public Book Get(string id)
+        {
+            ObjectId docId = new ObjectId(id);
+            return _books.Find(book => book.id == docId).FirstOrDefault();
+        }
+
+        public Book Create(Book book)
+        {
+            _books.InsertOne(book);
+            return book;
+        }
+
+        public void Update(string id,Book bookIn)
+        {
+            ObjectId docId = new ObjectId(id);
+            _books.ReplaceOne(book => book.id == docId, bookIn);
+        }
+
+        public void Remove(Book bookIn)
+        {
+            _books.DeleteOne(book => book.id == bookIn.id);
+        }
+
+        public void Remove(ObjectId id)
+        {
+            _books.DeleteOne(book => book.id == id);
+        }
+    }
+}
